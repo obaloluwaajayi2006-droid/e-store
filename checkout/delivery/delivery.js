6e07c18e6dcd055ad48bbaa68ba838c5ba299283
emmanuel123 <obaloluwaajayi2006@gmail.com>
2025-11-21 12:15:01 -0800

    made the address page functiona;

--- checkout/delivery/delivery.js
+++ checkout/delivery/delivery.js
@@ -1,1 +1,6 @@
-import { addressSave } from "../adress-page/address";
+let addressInfo = JSON.parse(localStorage.getItem('addressData')) || [];
+
+console.log(addressInfo.fName, addressInfo.lName)
+
+deliveryName.innerHTML = addressInfo.fName + ' ' + addressInfo.lName;
+deliveryAddress.innerHTML = addressInfo.add;
