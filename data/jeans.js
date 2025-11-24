0a75e1adbe89fd79a78327f9251a9499d8366ebc
emmanuel123 <obaloluwaajayi2006@gmail.com>
2025-11-24 15:57:44 -0800

    ..

--- data/jeans.js
+++ data/jeans.js
@@ -1,73 +1,73 @@
 export const jeans = [
   {
     id: "jeans-001",
-    productName: "Graphical tee shirts",
+    productName: "Skinny Jeans - Tight-fitting, stretch denim, modern look",
     image: "/images/shirt/Graphic-tees.jpg",
     price: 200
   },
   {
     id: "jeans-002",
-    productName: "Button-up shirts",
+    productName: "Slim Fit Jeans",
     image: "/images/shirt/Button-Up-Shirt.jpg",
     price: 250
   },
   {
     id: "jeans-003",
-    productName: "Oversized tee shirt",
+    productName: "Straight Leg Jeans - Classic straight-from-hip-to-ankle silhouette.",
     image: "/images/shirt/Oversized-T-Shirt.webp",
     price: 1500
   },
   {
     id: "jeans-004",
-    productName: "Sleeveless shirts or tank tops",
+    productName: "Bootcut Jeans",
     image: "/images/shirt/Sleeveless-Shirt.jpg",
     price: 750
   },
   {
     id: "jeans-005",
-    productName: "V-neck polo for men",
+    productName: "Flared Jean - Wide flare from knee downward — 70’s inspired.",
     image: "/images/shirt/V-Neck-Shirt.jpg",
     price: 100
   },
   {
     id: "jeans-006",
-    productName: "Flannel shirt",
+    productName: "Mom Jeans - High-waisted, loose around the hips, vintage style.",
     image: "/images/shirt/Flannel-Shirt.jpg",
     price: 150
   },
   {
     id: "jeans-007",
-    productName: "Polo shirts",
+    productName: "Boyfriend Jeans - Relaxed, baggy, slightly loose; comfortable fit.",
     image: "/images/shirt/Polo-Shirt.jpg",
     price: 800
   },
   {
     id: "jeans-008",
-    productName: "Henley shirts for adult men",
+    productName: "Ripped/Distressed Jeans - Torn or frayed sections for streetwear look.",
     image: "/images/shirt/Henley-Shirt.jpg",
     price: 2000
   },
   {
     id: "jeans-009",
-    productName: "Cropped flannel tee shirt",
+    productName: "Baggy Jeans - Oversized, wide- leg streetwear style.",
     image: "/images/shirt/cropped-tees-shirts.jpg",
     price: 100
   },
   {
     id: "jeans-010",
-    productName: "Baseball raglan shirt",
+    productName: "Cargo Jeans - with multiple pockets and utility style.",
     image: "/images/shirt/baseball-raglan-shirt.jpg",
     price: 100
   },
   {
     id: "jeans-011",
-    productName: "Classic tee shirts",
+    productName: "Wide-Leg Jeans",
     image: "/images/shirt/cropped-tees-shirts.jpg",
     price: 100
   },
   {
     id: "jeans-012",
-    productName: "Long sleeve round shirt",
+    productName: "Patchwork Jeans - with stitched patches — designer aesthetic.",
     image: "/images/shirt/Long-Sleeve-Shirt.jpg",
     price: 100
   }];
