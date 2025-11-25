f230e6427947d49af14e21529165c3f8451eaa25
emmanuel123 <obaloluwaajayi2006@gmail.com>
2025-11-24 16:02:43 -0800

    jeans

--- data/jeans.js
+++ data/jeans.js
@@ -2,72 +2,72 @@ export const jeans = [
   {
     id: "jeans-001",
     productName: "Skinny Jeans - Tight-fitting, stretch denim, modern look",
-    image: "/images/shirt/Graphic-tees.jpg",
+    image: "/images/jeans/Skinny-Jeans.jpg",
     price: 200
   },
   {
     id: "jeans-002",
     productName: "Slim Fit Jeans",
-    image: "/images/shirt/Button-Up-Shirt.jpg",
+    image: "/images/jeans/Slim-Fit-Jeans.jpg",
     price: 250
   },
   {
     id: "jeans-003",
     productName: "Straight Leg Jeans - Classic straight-from-hip-to-ankle silhouette.",
-    image: "/images/shirt/Oversized-T-Shirt.webp",
+    image: "/images/jeans/Straight-Leg-Jeans.jpg",
     price: 1500
   },
   {
     id: "jeans-004",
     productName: "Bootcut Jeans",
-    image: "/images/shirt/Sleeveless-Shirt.jpg",
+    image: "/images/jeans/Bootcut-Jeans.jpg",
     price: 750
   },
   {
     id: "jeans-005",
     productName: "Flared Jean - Wide flare from knee downward — 70’s inspired.",
-    image: "/images/shirt/V-Neck-Shirt.jpg",
+    image: "/images/jeans/Flared-Jean.jpg",
     price: 100
   },
   {
     id: "jeans-006",
     productName: "Mom Jeans - High-waisted, loose around the hips, vintage style.",
-    image: "/images/shirt/Flannel-Shirt.jpg",
+    image: "/images/jeans/Mom-Jeans.jpg",
     price: 150
   },
   {
     id: "jeans-007",
     productName: "Boyfriend Jeans - Relaxed, baggy, slightly loose; comfortable fit.",
-    image: "/images/shirt/Polo-Shirt.jpg",
+    image: "/images/jeans/Boyfriend-Jeans.jpg",
     price: 800
   },
   {
     id: "jeans-008",
     productName: "Ripped/Distressed Jeans - Torn or frayed sections for streetwear look.",
-    image: "/images/shirt/Henley-Shirt.jpg",
+    image: "/images/jeans/Ripped-or-Distressed-Jeans.jpg",
     price: 2000
   },
   {
     id: "jeans-009",
     productName: "Baggy Jeans - Oversized, wide- leg streetwear style.",
-    image: "/images/shirt/cropped-tees-shirts.jpg",
+    image: "/images/jeans/Baggy-Jeans.jpg",
     price: 100
   },
   {
     id: "jeans-010",
     productName: "Cargo Jeans - with multiple pockets and utility style.",
-    image: "/images/shirt/baseball-raglan-shirt.jpg",
+    image: "/images/jeans/Cargo-Jeans.jpg",
     price: 100
   },
   {
     id: "jeans-011",
     productName: "Wide-Leg Jeans",
-    image: "/images/shirt/cropped-tees-shirts.jpg",
+    image: "/images/jeans/Wide-Leg-Jeans.jpg",
     price: 100
   },
   {
     id: "jeans-012",
     productName: "Patchwork Jeans - with stitched patches — designer aesthetic.",
-    image: "/images/shirt/Long-Sleeve-Shirt.jpg",
+    image: "/images/jeans/Patchwork-Jeans.jpg",
     price: 100
   }];
